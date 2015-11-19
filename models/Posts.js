69c1386c6be4351c5af74e73ef45a4cabd6db6f4
Balaji S <balaaagi@gmail.com>
2015-11-19 08:56:41 +0530

    Added Date type to Posts and Order

--- models/Posts.js
+++ models/Posts.js
@@ -4,7 +4,8 @@ var PostSchema=new mongoose.Schema({
 	title:String,
 	url:String,
 	hostname:String,
-	upvotes: {type: Number, default: 0}
+	upvotes: {type: Number, default: 0},
+	createdAt:{type:Date,default:Date.now}
 });
 
 PostSchema.methods.upvote = function(pv) {
